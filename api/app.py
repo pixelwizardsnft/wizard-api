7a5d2e96e9a2b35bd8ba46b97f1c05c39e5f75f9
Berny <berny.ust@gmail.com>
2022-12-30 17:40:00 +0100

    sqlite > postgresql

--- api/app.py
+++ api/app.py
@@ -1,30 +1,78 @@
-import sqlite3
-#import knowledge
+from decouple import config
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import psycopg2
+import psycopg2.extras
+import urllib.parse as up
+import json
+from pycoingecko import CoinGeckoAPI
 
+cg = CoinGeckoAPI()
+DATABASE_URL = config('DATABASE_URL')
+
+### BASE FUNCTIONS
 def connect_to_db():
-    con = sqlite3.connect('api/tokenKnowledge.db')
-    print("connected")
+    con = None
+    up.uses_netloc.append("postgres")
+    url = up.urlparse(DATABASE_URL)
+    con = psycopg2.connect(database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
     return con
 
+def save_knowledge():
+    tokens = "bitcoin,ethereum,stargaze,osmosis,cosmos,terrausd,terra-luna-2,ripple,uniswap,solana,the-sandbox,thorchain,near,mirror-protocol,decentraland,chainlink,internet-computer,polkadot,dogecoin,pancakeswap-token,binancecoin,avalanche-2,arweave,cardano"
+    listKnowledge = cg.get_coins_markets(ids=tokens, vs_currency='usd', order='market_cap_desc', price_change_percentage='24h')
+    for knowledge in listKnowledge:
+        jKnowledge = json.dumps(knowledge)
+        log_knowledge(jKnowledge)
+
+def log_knowledge(message):
+    parsed = json.loads(message)
+    
+    symbol = "'"+parsed["symbol"].upper()+"'"
+    price = float(parsed["current_price"])
+    priceHigh = float(parsed["high_24h"])
+    priceLow = float(parsed["low_24h"])
+    changePer = float(parsed["price_change_percentage_24h"])
+    time = "'"+parsed["last_updated"]+"'"
+
+    try:
+        con = connect_to_db()
+        cur = con.cursor()
+        cur.execute(f''' INSERT INTO tokenknowledge (symbol, price, pricehigh, pricelow, changeper, lastupdated) 
+                         VALUES ({symbol}, {price}, {priceHigh}, {priceLow}, {changePer}, {time}); ''')
+        con.commit()
+        con.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
+
+### API FUNCTIONS
 def get_all_knowledge():
     allknowledge = []
     try:
         con = connect_to_db()
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute("SELECT * FROM TokenKnowledge")
+        print("connected")
+        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT * FROM tokenknowledge")
         rows = cur.fetchall()
+
+        print(rows)
 
         for i in rows:
             knowledge = {}
-            knowledge["symbol"] = i["Symbol"]
-            knowledge["price"] = i["Price"]
-            knowledge["priceHigh"] = i["PriceHigh"]
-            knowledge["priceLow"] = i["PriceLow"]
-            knowledge["changePer"] = i["ChangePer"]
-            knowledge["timestamp"] = i["Timestamp"]
+            knowledge["symbol"] = i["symbol"]
+            knowledge["price"] = i["price"]
+            knowledge["priceHigh"] = i["pricehigh"]
+            knowledge["priceLow"] = i["pricelow"]
+            knowledge["changePer"] = i["changeper"]
+            knowledge["timestamp"] = i["lastupdated"]
             allknowledge.append(knowledge)
     except:
         allknowledge = []
@@ -35,18 +83,17 @@ def get_knowledge_by_token(token):
     knowledge = {}
     try:
         con = connect_to_db()
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute("SELECT * FROM TokenKnowledge WHERE symbol =?", (token,))
+        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(f"SELECT * FROM tokenknowledge WHERE symbol ='{token}'")
         row = cur.fetchone()
 
         knowledge = {}
-        knowledge["symbol"] = row["Symbol"]
-        knowledge["price"] = row["Price"]
-        knowledge["priceHigh"] = row["PriceHigh"]
-        knowledge["priceLow"] = row["PriceLow"]
-        knowledge["changePer"] = row["ChangePer"]
-        knowledge["timestamp"] = row["Timestamp"]    
+        knowledge["symbol"] = row["symbol"]
+        knowledge["price"] = row["price"]
+        knowledge["priceHigh"] = row["pricehigh"]
+        knowledge["priceLow"] = row["pricelow"]
+        knowledge["changePer"] = row["changeper"]
+        knowledge["timestamp"] = row["lastupdated"]
     except:
         knowledge = {}
     
@@ -59,10 +106,15 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 def home():
     return 'Welcome to the Pixel Wizard "Book of Knowledge"!'
 
-@app.route('/api/knowledge', methods=['GET'])
+@app.route('/api/v1/knowledge', methods=['GET'])
 def api_get_all_knowledge():
     return jsonify(get_all_knowledge())
 
-@app.route('/api/knowledge/<token>', methods=['GET'])
+@app.route('/api/v1/knowledge/<token>', methods=['GET'])
 def api_get_knowledge(token):
     return jsonify(get_knowledge_by_token(token))
+
+@app.route('/api/v1/updateKnowledge', methods=['GET', 'POST'])
+def api_update_knowledge():
+    save_knowledge()
+    return 'OK', 200
